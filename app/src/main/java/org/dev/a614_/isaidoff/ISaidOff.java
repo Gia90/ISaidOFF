148270102a93b252d07d334109c80cc40749bab2
Gia90 <Gia90@users.noreply.github.com>
2018-06-18 22:52:51 +0200

    First working version, asking to unlock if keyguard is ON

---
+++ app/src/main/java/org/dev/a614_/isaidoff/ISaidOff.java
@@ -0,0 +1,31 @@
+package org.dev.a614_.isaidoff;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.callMethod;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+
+public class ISaidOff implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.systemui")) return;
+
+        final Class<?> cellularTileClass = findClass("com.android.systemui.qs.tiles.CellularTile", lpparam.classLoader);
+
+        // https://github.com/aosp-mirror/platform_frameworks_base/blob/d18ed49f9dba09b85782c83999a9103dec015bf2/packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+        // Hooking method: private void showDisableDialog()
+        findAndHookMethod(cellularTileClass, "showDisableDialog", new XC_MethodReplacement() {
+
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                Object mDataController = getObjectField(param.thisObject, "mDataController");
+                callMethod(mDataController, "setMobileDataEnabled", false);
+                return null;
+            }
+        });
+    }
+}
