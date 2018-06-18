73ac58a946fd761b9ce9bdbc2932b64f6f275a9a
Gia90 <Gia90@users.noreply.github.com>
2018-06-18 23:03:57 +0200

    Ignore secure lockscreen

--- app/src/main/java/org/dev/a614_/isaidoff/ISaidOff.java
+++ app/src/main/java/org/dev/a614_/isaidoff/ISaidOff.java
@@ -17,13 +17,19 @@ public class ISaidOff implements IXposedHookLoadPackage {
         final Class<?> cellularTileClass = findClass("com.android.systemui.qs.tiles.CellularTile", lpparam.classLoader);
 
         // https://github.com/aosp-mirror/platform_frameworks_base/blob/d18ed49f9dba09b85782c83999a9103dec015bf2/packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
-        // Hooking method: private void showDisableDialog()
-        findAndHookMethod(cellularTileClass, "showDisableDialog", new XC_MethodReplacement() {
+        // Hooking method: protected void handleClick()
+        findAndHookMethod(cellularTileClass, "handleClick", new XC_MethodReplacement() {
 
             @Override
             protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+
                 Object mDataController = getObjectField(param.thisObject, "mDataController");
-                callMethod(mDataController, "setMobileDataEnabled", false);
+
+                if ( (Boolean)callMethod(mDataController, "isMobileDataEnabled") ) {
+                    callMethod(mDataController, "setMobileDataEnabled", false);
+                } else {
+                    callMethod(mDataController, "setMobileDataEnabled", true);
+                }
                 return null;
             }
         });
